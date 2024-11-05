40039623e8a5cd5a6dd2cc43b36aa76e8e279ed0
keenan <765588453@qq.com>
2024-11-06 01:28:27 +0800

    bug修改

--- wxcloudrun/asgi.py
+++ wxcloudrun/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from your_app_name import consumers
+from .consumers import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
--- wxcloudrun/wsgi.py
+++ wxcloudrun/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from.routing import websocket_urlpatterns
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wxcloudrun.settings')
-
-application = get_wsgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
