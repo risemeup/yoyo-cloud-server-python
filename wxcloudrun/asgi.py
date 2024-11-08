65aed3690066ea93f61530dd7ed3405e846cec83
keenan <765588453@qq.com>
2024-11-09 03:01:31 +0800

    镜像环境适配

--- wxcloudrun/asgi.py
+++ wxcloudrun/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from .consumers import consumers
+from .consumers import EchoConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            path('ws/', consumers.EchoConsumer.as_asgi()),
+            path('ws/', EchoConsumer.as_asgi()),
         ])
     ),
 })
