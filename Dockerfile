9e0ee3911e52963945e36f2efded0bc51aad482d
keenan <765588453@qq.com>
2024-11-14 02:43:01 +0800

    初始化

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apk add ca-certificates
 
 # 选用国内镜像源以提高下载速度
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tencent.com/g' /etc/apk/repositories \
-&& apk add --update --no-cache python3 py3-pip gcc musl-dev linux-headers python3-dev libffi-dev\
+&& apk add --update --no-cache python3 py3-pip \
 && rm -rf /var/cache/apk/*
 
 # 拷贝当前项目到/app目录下(.dockerignore中文件除外)
--- wxcloudrun/asgi.py
+++ wxcloudrun/asgi.py
@@ -1,17 +1,16 @@
+"""
+ASGI config for wxcloudrun project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from .consumers import EchoConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wxcloudrun.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            path('ws/', EchoConsumer.as_asgi()),
-        ])
-    ),
-})
+application = get_asgi_application()
