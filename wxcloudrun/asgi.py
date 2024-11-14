0f4dee4eb80ce25cd12d0bac68edd437f6e3e0a4
risemeup <48812036+risemeup@users.noreply.github.com>
2024-11-15 01:48:17 +0800

    websocket (#4)
    * 镜像环境适配
    
    * 更换端口

--- wxcloudrun/asgi.py
+++ wxcloudrun/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from .consumers import EchoConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wxcloudrun.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path('ws/', EchoConsumer.as_asgi()),
+        ])
+    ),
+})
