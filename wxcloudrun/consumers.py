7a6b07c643f7c9f90239e21c3fd5839e40de5c13
keenan <765588453@qq.com>
2024-11-25 01:43:46 +0800

    发送数据bug

--- wxcloudrun/consumers.py
+++ wxcloudrun/consumers.py
@@ -31,49 +31,6 @@ class Message:
         self.options = options
         self.multiSelect = multiSelect
 
-def receive_message(raw_data: str) -> Message:
-    """
-    接收消息函数，从原始数据字符串中解析出Message对象
-    :param raw_data: 原始数据字符串，假设其格式符合Message类的结构要求
-    :return: 解析出的Message对象
-    """
-    data_dict = eval(raw_data)  # 这里假设原始数据可以通过eval转换为字典，实际应用中可能需要更安全的解析方式
-
-    role = data_dict.get('role')
-    msg_type = data_dict.get('type')
-    id = data_dict.get('id')
-    timestamp = data_dict.get('timestamp')
-    text = data_dict.get('text')
-    options_data = data_dict.get('options')
-    multiSelect = data_dict.get('multiSelect')
-
-    options = None
-    if options_data:
-        options = [Option(opt['id'], opt['label']) for opt in options_data]
-
-    return Message(role, msg_type, id, timestamp, text, options, multiSelect)
-
-
-def send_message(message: Message) -> str:
-    """
-    发送消息函数，将Message对象转换为字符串以便发送
-    :param message: 要发送的Message对象
-    :return: 转换后的字符串
-    """
-    message_dict = {
-        'role': message.role,
-        'type': message.type,
-        'id': message.id,
-        'timestamp': message.timestamp,
-        'text': message.text
-    }
-    if message.options:
-        message_dict['options'] = [{'id': opt.id, 'label': opt.label} for opt in message.options]
-    if message.multiSelect:
-        message_dict['multiSelect'] = message.multiSelect
-
-    return str(message_dict)
-
 def create_uuid() -> str:
     return str(uuid.uuid4())
 
@@ -90,7 +47,7 @@ class EchoConsumer(AsyncWebsocketConsumer):
             timestamp=get_cur_time_str(),
             text="你好哇！我是你的旅行助手yoyo！请告诉我你想去哪旅行？"
         )
-        send_message(mess)
+        self.send_message(mess)
         logger.info('connect success!')
 
     async def disconnect(self, close_code):
@@ -99,7 +56,51 @@ class EchoConsumer(AsyncWebsocketConsumer):
     # 直接返回接收到消息
     async def receive(self, text_data):
         logger.info('receive: {}'.format(text_data))
-        mess = receive_message(text_data)
+        mess = self.receive_message(text_data)
         mess.role = Role.SERVER
-        send_message(mess)
-        logger.info('send: {}'.format(mess))
+        self.send_message(mess)
+        
+    
+    async def send_message(self, message: Message):
+        """
+        发送消息函数，将Message对象转换为字符串以便发送
+        :param message: 要发送的Message对象
+        :return: 转换后的字符串
+        """
+        message_dict = {
+            'role': message.role,
+            'type': message.type,
+            'id': message.id,
+            'timestamp': message.timestamp,
+            'text': message.text
+        }
+        if message.options:
+            message_dict['options'] = [{'id': opt.id, 'label': opt.label} for opt in message.options]
+        if message.multiSelect:
+            message_dict['multiSelect'] = message.multiSelect
+        
+        await self.send(text_data=json.dumps(message))
+        logger.info('send: {}'.format(message_dict))
+    
+    def receive_message(self, raw_data: str) -> Message:
+        """
+        接收消息函数，从原始数据字符串中解析出Message对象
+        :param raw_data: 原始数据字符串，假设其格式符合Message类的结构要求
+        :return: 解析出的Message对象
+        """
+        data_dict = eval(raw_data)  # 这里假设原始数据可以通过eval转换为字典，实际应用中可能需要更安全的解析方式
+
+        role = data_dict.get('role')
+        msg_type = data_dict.get('type')
+        id = data_dict.get('id')
+        timestamp = data_dict.get('timestamp')
+        text = data_dict.get('text')
+        options_data = data_dict.get('options')
+        multiSelect = data_dict.get('multiSelect')
+
+        options = None
+        if options_data:
+            options = [Option(opt['id'], opt['label']) for opt in options_data]
+
+        return Message(role, msg_type, id, timestamp, text, options, multiSelect)
+
