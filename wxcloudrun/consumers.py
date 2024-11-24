ddb7ec237717b078d7758d8532f828678dcfc8d6
keenan <765588453@qq.com>
2024-11-24 17:34:47 +0800

    普通文本接口

--- wxcloudrun/consumers.py
+++ wxcloudrun/consumers.py
@@ -1,6 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 import logging
+import uuid
+from datetime import datetime
 logger = logging.getLogger('log')
 
 class EchoConsumer(AsyncWebsocketConsumer):
@@ -13,8 +15,21 @@ class EchoConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         logger.info('receive: {}'.format(text_data))
+        # 生成一个随机的UUID（通用唯一识别码），版本为4
+        unique_id = uuid.uuid4()
+
+        # 将UUID转换为字符串形式
+        unique_string_id = str(unique_id)
+
+        formatted_time_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
         message = {
-            'message': text_data
+            "type": "text",
+            "id": unique_string_id,
+            "timestamp": formatted_time_string,
+            "content": {
+                "text": text_data
+            }
         }
         await self.send(text_data=json.dumps(message))
         logger.info('send: {}'.format(message))
