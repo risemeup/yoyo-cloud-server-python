a408c40b5e5b60439a98edbedfc27e2abfe8da7f
keenan <765588453@qq.com>
2024-11-26 00:37:44 +0800

    固定消息发送

--- wxcloudrun/consumers.py
+++ wxcloudrun/consumers.py
@@ -14,7 +14,12 @@ class EchoConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         logger.info('receive: {}'.format(text_data))
         message = {
-            'message': 'hello'
+            "type": "text",
+            "id": "1111",
+            "timestamp": "2024-11-24 11:00:00",
+            "content": {
+                "text": '123'
+                }
         }
         await self.send(text_data=json.dumps(message))
         logger.info('send: {}'.format(message))
