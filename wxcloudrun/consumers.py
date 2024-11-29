e1c093dd11a4d69c9fcaa73a22ecfd1a02e70a40
lcs <lcs@tencent.com>
2024-11-29 22:45:57 +0800

    删除无效代码

--- wxcloudrun/consumers.py
+++ wxcloudrun/consumers.py
@@ -80,89 +80,9 @@ class EchoConsumer(AsyncWebsocketConsumer):
         :return: 转换后的字符串
         """
         try:
-            # message_str = self.message_to_json(message)
             message_str = json.dumps(asdict(message))
             await self.send(text_data=message_str)
             logger.info('send: {}'.format(message_str))
         except Exception as e:
             logger.error(f"发生异常: {e}")
-    
-    def json_to_message(self, received_data_str: str) -> Message:
-        """
-        Parses a JSON-encoded string into a Message object.
-
-        This function takes a string representing JSON data and attempts to parse it
-        into a Message object. If the input string is not a valid JSON, a ValueError
-        is raised. The JSON data must contain the fields 'role', 'type', 'id',
-        'timestamp', and 'content'. The 'content' field can either be a text or
-        options. If the 'content' includes 'text', it is parsed as TextContent. If it
-        includes 'options', it is parsed as OptionContent with a list of options and
-        a multiSelect flag.
-
-        :param received_data_str: The JSON string to be parsed.
-        :return: A Message object constructed from the provided JSON data.
-        :raises ValueError: If the provided string is not valid JSON or contains
-                            invalid content data.
-        """
-        try:
-            logger.info('received_data_str: {}'.format(received_data_str))
-            received_data = json.loads(received_data_str)
-        except json.JSONDecodeError as e:
-            raise ValueError(f"无法将接收到的数据转换为JSON格式: {e}")
-
-        role = received_data.get('role')
-        msg_type = received_data.get('type')
-        id = received_data.get('id')
-        timestamp = received_data.get('timestamp')
-        content_data = received_data.get('content')
-
-        if msg_type == MessType.TEXT.value:
-            content = TextContent(content_data['text'])
-        elif msg_type == MessType.OPTIONS.value:
-            options = [Option(opt['id'], opt['label']) for opt in content_data['options']]
-            multiSelect = content_data['multiSelect']
-            content = OptionContent(options, multiSelect)
-        else:
-            raise ValueError("无效的接收数据内容")
-
-        return Message(role, msg_type, id, timestamp, content)
-
-    def message_to_json(self, message: Message) -> str:
-        """
-        Converts a Message object to a JSON string.
-
-        This function takes a Message object and converts it to a JSON string.
-        The JSON string contains the fields 'role', 'type', 'id', 'timestamp',
-        and 'content'. The 'content' field depends on the type of the message.
-        If the message type is 'text', the content field contains a text string.
-        If the message type is 'options', the content field contains a list of
-        options and a multiSelect flag.
-
-        :param message: The Message object to be converted to a JSON string.
-        :return: A JSON string representing the Message object.
-        """
-        message_dict = {
-            'role': message.role,
-            'type': message.type,
-            'id': message.id,
-            'timestamp': message.timestamp,
-        }
-
-        content_dict = {}
-        if isinstance(message.content, TextContent):
-            content_dict['text'] = message.content.text
-        elif isinstance(message.content, OptionContent):
-            options_list = []
-            for option in message.content.options:
-                options_list.append({
-                    'id': option.id,
-                    'label': option.label
-                })
-            content_dict['options'] = options_list
-            content_dict['multiSelect'] = message.content.multiSelect
-
-        logger.info('bbb message_dict: {}'.format(message_dict))
-        message_dict['content'] = content_dict
-        logger.info('ccc message_dict: {}'.format(message_dict))
-
-        return json.dumps(message_dict)
+            
